9f976c7652c968dbd2df7395641403161c2b03c0
joaoraes <moraes.santos07@escola.pr.gov.br>
2025-08-14 14:38:47 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,31 @@
+criaCartao(
+    'História',
+    'Quem foi o primeiro presidente do Brasil?',
+    'Foi Marechal Deodoro da Fonseca.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual o maior oceano?',
+    'Oceano Pacífico'
+)
+criaCartao(
+    'Geografia',
+    'Qual o maior país do mundo?',
+    'É a Rússia.'
+)
+criaCartao(
+    'Filosofia',
+    'O que é realidade?',
+    'Esta pergunta explora a natureza da existência e se o que percebemos é realmente o que é.'
+)
+criaCartao(
+    'Biologia',
+    'Quantos ossos tem no corpo humano?',
+    '206.'
+)
+criaCartao(
+    'Química',
+    'Qual o primeiro elemento químico da tebal periódica?',
+    'Hidrogênio.'
+)
